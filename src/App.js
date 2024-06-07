98f1cb3b28fc0caf27d530be2156c6864a5c3396
Ashwani Kumar <ashwani.kumar@enabledata.com>
2024-06-07 19:14:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { useReactPWAInstall } from "react-pwa-install";
+import myLogo from "./img/logo.png";
 function App() {
+  const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
+
+  const handleClick = () => {
+    pwaInstall({
+      title: "Install Well Way",
+      logo: myLogo,
+      features: (
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+          <li>Works offline</li>
+        </ul>
+      ),
+      description: "This is a very good app that does a lot of useful stuff. ",
+    })
+      .then(() => alert("App installed successfully!"))
+      .catch(() => alert("User opted out from installing"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: '20px'}}>
+      <h1>Welcome to WellWay </h1>
+      <p>The Install App button would show only if the app is not already installed on the device. </p>
+      {supported() && !isInstalled() && (
+        <button type="button" onClick={handleClick}>
+          Install App
+        </button>
+      )}
     </div>
   );
 }
